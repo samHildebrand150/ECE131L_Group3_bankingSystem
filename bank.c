36c7a2325c01755f3bc2a89b7e00d9567836cc87
samHildebrand150 <hildebrandsamuel807@gmail.com>
2026-04-27 14:39:56 -0600

    Update bank.c
    I Added the authorization function

--- bank.c
+++ bank.c
@@ -7,3 +7,27 @@ struct account // this creates the structure of an account :)
     int pin;
     double balance;
 };
+//prototypes for functions
+int auth(struct account bankUsers[], int totalUsers);
+int main()
+{
+    //this creates 3 accounts that each have an ID, PIN, Balance.. all in that order
+    struct account bankUsers[3] ={
+        {101, 1234, 100.00},
+        {102, 4321, 200.00},
+        {103, 1111, 300.00}
+    };
+
+    int totalUsers = 3;
+   
+    //this will tell you what user logged in through the auth() function
+    int userIdx = auth(bankUsers, totalUsers);
+
+    // this is a safegaurd to end program if user fails to log in
+    if(userIdx == -1)
+    {
+        printf("too many failed attempts goodbye!\n");
+        return 0;
+    }
+
+    double *pBalance = &bankUsers[userIdx].balance;
