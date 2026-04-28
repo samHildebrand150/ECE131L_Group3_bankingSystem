9d714ac6f3043444c5ed4a488ff1532b32aadf53
WeberCodez <thomas@sierrablancabrewery.com>
2026-04-27 19:31:44 -0600

    Fix greeting message and streamline return values
    Corrected greeting message and simplified return logic.

--- finalBank.c
+++ finalBank.c
@@ -187,15 +187,13 @@ int auth(struct account bankUsers[], int totalUsers)
 {
     int tempID, tempPIN, attempts =0;
 
-    printf("hello what is your account number\n");
+    printf("Hello, what is your account number?\n");
     scanf("%d", &tempID);
-    
-    int found = 0;
+
     for(int i=0;  i < totalUsers; i++)
     {
         if(tempID == bankUsers[i].id)
         {
-            found = 1;
             printf("Account found!\nPlease enter PIN:");
             scanf("%d", &tempPIN);
 
@@ -209,10 +207,9 @@ int auth(struct account bankUsers[], int totalUsers)
                 printf("Incorrect PIN, please try again \n");
                 scanf("%d", &tempPIN);
             }
-            return -1;
         }    
     }
-    return found ? -1 : -2;
+    return -1;
 }
 void transfer(struct account bankUsers[], int totalUsers, int userIdx,FILE *pF){
     // ask where the money is going
